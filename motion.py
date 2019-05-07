7c9e68c44d7ad396fb70c778021e9a45ab49f454
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2019-05-07 15:58:42 +1000

    rename spin_around, add drive function
    currently tuned with magic number until PID is tested

--- motion.py
+++ motion.py
@@ -1,12 +1,12 @@
 import StarLAB
 import time
 
-rover = StarLAB.Connect(IP="****.****.****.****")  # TODO update this
+rover = StarLAB.Connect(IP="192.168.1.12")
 rover.enableRover()
 
 
 def spin(motor_value: int, duration: float) -> None:
-    """\
+    """
     Run the motors in opposite directions to turn on the spot.
     Param: motor_value - a posative or negative percentage.
     Param: duration - time in seconds to turn for.
@@ -25,4 +25,20 @@ def spin(motor_value: int, duration: float) -> None:
         pass
 
 
-spin(50, 3)
+def drive(motor_value: int, duration: float):
+    if motor_value > 100 or motor_value < -100:
+        # confirm the values are within bounds
+        raise ValueError
+    start_time = time.monotonic()
+    current_time = start_time
+    while current_time < (start_time + duration):
+        current_time = time.monotonic()
+        rover.motors.setMotorPower(motor_value - 10, motor_value)
+    else:
+        rover.motors.setMotorPower(0, 0)
+        # next state goes here
+        pass
+
+
+# spin(50, 3)
+drive(100, 13)
